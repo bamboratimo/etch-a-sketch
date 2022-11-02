5bce7013ce47bc2674aeefc5910d2abd574aa5c3
Timo Rautiainen <timo.rautiainen94@gmail.com>
2022-11-02 21:43:22 +0200

    add change color on hover

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.classList.add("square");
+    div.addEventListener("mouseover", changeColor);
     container.appendChild(div);
+}
+
+function changeColor(e) {
+    e.target.classList.add("change");
 }
